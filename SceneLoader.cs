908fad6d5170231849689262a93281653cc45025
Exanite <exanite78012@gmail.com>
2024-02-17 15:10:15 -0500

    Rename EnableSceneObjects for consistency

--- SceneLoader.cs
+++ SceneLoader.cs
@@ -133,7 +133,7 @@ namespace Exanite.SceneManagement
 
                 var parentContainers = parentSceneLoaders.Select(loader => loader.SceneContext.Container).ToList();
                 SceneLoadManager.SetSceneContextParameters(parentContainers.Count == 0 ? null : parentContainers);
-                ActivateSceneObjects();
+                EnableSceneObjects();
                 SceneLoadManager.CleanupSceneContextParameters();
             }
             finally
@@ -180,7 +180,7 @@ namespace Exanite.SceneManagement
             }
         }
 
-        private void ActivateSceneObjects()
+        private void EnableSceneObjects()
         {
             if (sceneObjectsParent == null)
             {
