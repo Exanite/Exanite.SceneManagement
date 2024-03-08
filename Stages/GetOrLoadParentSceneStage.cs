9b7d19afa3d77172d4d5385a5edb9d4c29be7326
Exanite <exanite78012@gmail.com>
2024-03-07 21:56:59 -0500

    Refactor code and add IsCompatible() method

--- Stages/GetOrLoadParentSceneStage.cs
+++ Stages/GetOrLoadParentSceneStage.cs
@@ -23,27 +23,27 @@ namespace Exanite.SceneManagement.Stages
             await UniTask.WaitWhile(() => SceneLoadManager.IsLoading);
 
             // Check to see if there are existing scenes compatible with being a parent of this scene
-            var existingScenes = SceneLoaderRegistry.SceneLoaders.Where(pair =>
-            {
-                var (_, sceneLoader) = pair;
-
-                return sceneLoader.Identifier == parentSceneIdentifier;
-            });
-
-            foreach (var (_, candidateSceneLoader) in existingScenes)
-            {
-                // Todo Replace with actual condition
-                if (true)
+            var existingScene = SceneLoaderRegistry.SceneLoaders.FirstOrDefault(pair =>
                 {
-                    return candidateSceneLoader;
-                }
+                    return IsCompatibleScene(pair.Value);
+                })
+                .Value;
+
+            if (existingScene != null)
+            {
+                return existingScene;
             }
 
             // Otherwise, create a new parent
-            var parentScene = await parentSceneIdentifier.Load(currentScene);
+            var parentScene = await parentSceneIdentifier.Load();
             var parentSceneLoader = SceneLoaderRegistry.SceneLoaders[parentScene];
 
             return parentSceneLoader;
+        }
+
+        protected virtual bool IsCompatibleScene(SceneLoader scene)
+        {
+            return scene.Identifier == parentSceneIdentifier;
         }
     }
 }
