ebfa50eae14fdaa82efac2a745be355bae09196c
Exanite <exanite78012@gmail.com>
2024-05-31 02:23:43 -0400

    Merge branch 'dev/scene-relations'

--- Stages/UsePeerSceneStage.cs
+++ Stages/UsePeerSceneStage.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
@@ -6,51 +7,109 @@ using UnityEngine.SceneManagement;
 namespace Exanite.SceneManagement.Stages
 {
     /// <summary>
-    /// Gets an existing parent scene or loads it if it is not loaded.
-    /// <para/>
-    /// Peer scenes are scenes that have to be loaded before this scene initializes.
+    /// Defines and automatically creates a scene relation.
+    /// When a scene loads, this scene load stage will attempt to look for an existing peer scene and load it if it isn't found.
+    /// Different <see cref="SceneRelationType">SceneRelationTypes</see> and other options can be used to modify how the relation works.
     /// </summary>
     public class UsePeerSceneStage : SceneLoadStage
     {
-        [SerializeField] private SceneIdentifier peerSceneIdentifier;
+        [SerializeField] protected SceneIdentifier peerSceneIdentifier;
+        [SerializeField] protected SceneRelationType relationType = SceneRelationType.Peer;
+
+        [Space]
+        [SerializeField] protected bool requireScene = true;
+        [SerializeField] protected bool createSceneIfDoesntExist = true;
+
+        public SceneIdentifier PeerSceneIdentifier
+        {
+            get => peerSceneIdentifier;
+            set => peerSceneIdentifier = value;
+        }
+
+        public SceneRelationType RelationType
+        {
+            get => relationType;
+            set => relationType = value;
+        }
+
+        public bool RequireScene
+        {
+            get => requireScene;
+            set => requireScene = value;
+        }
+
+        public bool CreateSceneIfDoesntExist
+        {
+            get => createSceneIfDoesntExist;
+            set => createSceneIfDoesntExist = value;
+        }
 
         public override async UniTask Load(Scene currentScene)
         {
-            await LoadInternal(currentScene);
+            var currentSceneInitializer = SceneInitializerRegistry.SceneInitializers[currentScene];
+
+            SceneInitializer peerSceneInitializer;
+            var existingPeerScene = await TryGetExistingPeerScene(currentSceneInitializer);
+            if (existingPeerScene.Exists)
+            {
+                peerSceneInitializer = existingPeerScene.Initializer;
+            }
+            else
+            {
+                if (createSceneIfDoesntExist)
+                {
+                    peerSceneInitializer = await LoadPeerScene(currentSceneInitializer);
+                }
+                else
+                {
+                    if (requireScene)
+                    {
+                        throw new Exception("Failed to find existing scene that is suitable to be this scene's peer scene");
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+
+            switch (relationType)
+            {
+                case SceneRelationType.Parent:
+                {
+                    currentSceneInitializer.AddParentSceneInitializer(peerSceneInitializer);
+
+                    break;
+                }
+            }
         }
 
-        protected virtual UniTask<SceneInitializer> LoadInternal(Scene currentScene)
-        {
-            return GetOrLoadScene(currentScene);
-        }
-
-        private async UniTask<SceneInitializer> GetOrLoadScene(Scene currentScene)
+        protected virtual async UniTask<(bool Exists, SceneInitializer Initializer)> TryGetExistingPeerScene(SceneInitializer currentScene)
         {
             // Wait for all scene load operations to complete
             await UniTask.WaitWhile(() => SceneLoader.IsLoading);
 
-            // Check to see if there are existing scenes compatible with being a parent of this scene
+            // Check if there are existing scenes compatible with being a peer of this scene
             var existingScene = SceneInitializerRegistry.SceneInitializers.FirstOrDefault(pair =>
                 {
-                    return IsCompatibleScene(pair.Value);
+                    return IsCompatiblePeerScene(pair.Value, currentScene);
                 })
                 .Value;
 
-            if (existingScene != null)
-            {
-                return existingScene;
-            }
-
-            // Otherwise, create a new parent
-            var parentScene = await peerSceneIdentifier.Load(LoadSceneMode.Additive);
-            var parentSceneInitializer = SceneInitializerRegistry.SceneInitializers[parentScene];
-
-            return parentSceneInitializer;
+            return (existingScene != null, existingScene);
         }
 
-        protected virtual bool IsCompatibleScene(SceneInitializer scene)
+        protected virtual async UniTask<SceneInitializer> LoadPeerScene(SceneInitializer currentScene)
         {
-            return scene.Identifiers.Find(identifier => identifier == peerSceneIdentifier);
+            var peerScene = await peerSceneIdentifier.Load(LoadSceneMode.Additive);
+            var peerSceneInitializer = SceneInitializerRegistry.SceneInitializers[peerScene];
+
+            return peerSceneInitializer;
+        }
+
+        protected virtual bool IsCompatiblePeerScene(SceneInitializer peerScene, SceneInitializer currentScene)
+        {
+            return peerScene != currentScene && peerScene.Identifiers.Find(identifier => identifier == peerSceneIdentifier);
         }
     }
 }
